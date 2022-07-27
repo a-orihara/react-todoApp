639cecc04c3b2d4d0a3148b30e7fa552f669e43d
a-orihara <ko.ty.es.ty.mk@gmail.com>
2022-07-27 20:34:57 +0900

    Modelファイルにvalidatesの設定。

--- backend/app/models/todo.rb
+++ backend/app/models/todo.rb
@@ -1,2 +1,5 @@
+# classモデル名でモデルを定義することができます。ここでは単数形になることに注意してください。
 class Todo < ApplicationRecord
+    # validates(:name, presence: true)と同義
+    validates :name, presence: true, length: { maximum: 12 }, unique: true
 end
