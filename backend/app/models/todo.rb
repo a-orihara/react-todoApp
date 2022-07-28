c657ba6bd709e7ce49572b217013bc04dc8671bd
a-orihara <ko.ty.es.ty.mk@gmail.com>
2022-07-28 14:39:28 +0900

    seedファイルを作成

--- backend/app/models/todo.rb
+++ backend/app/models/todo.rb
@@ -1,5 +1,6 @@
 # classモデル名でモデルを定義することができます。ここでは単数形になることに注意してください。
 class Todo < ApplicationRecord
     # validates(:name, presence: true)と同義
-    validates :name, presence: true, length: { maximum: 12 }, unique: true
+    # validatesはクラスメソッド.Modelの定義でよく目にする、attr_accessorやhas_manyも同様にクラスメソッド
+    validates :name, presence: true, length: { maximum: 20 }
 end
